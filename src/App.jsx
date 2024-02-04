a521db4e9d58d8829155336f3063da6f806d326b
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-02-04 16:16:56 +0530

    testing ai-photo-booth-wella

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CaptureImagePage from "./pages/CaptureImagePage";
+import KnowMorePage from "./pages/KnowMorePage";
+import AvatarPage from "./pages/AvatarPage";
+import GeneratedImagePage from "./pages/GeneratedImagePage";
+import SelectGender from "./pages/SelectGender";
+
+export default function App() {
+  const [capturedImage, setCapturedImg] = useState();
+  const [generatedImage, setGeneratedImage] = useState();
+  const [selectedGender, setSelectedGender] = useState();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/select-gender"
+          element={
+            <SelectGender
+              setSelectedGender={setSelectedGender}
+              selectedGender={selectedGender}
+            />
+          }
+        />
+        <Route
+          path="/capture-image"
+          element={<CaptureImagePage setCapturedImg={setCapturedImg} />}
+        />
+        <Route path="/know-more" element={<KnowMorePage />} />
+        <Route
+          path="/avatar"
+          element={
+            <AvatarPage
+              capturedImage={capturedImage}
+              setGeneratedImage={setGeneratedImage}
+              selectedGender={selectedGender}
+              generatedImage={generatedImage}
+            />
+          }
+        />
+        <Route
+          path="/generated-image"
+          element={<GeneratedImagePage generatedImage={generatedImage} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/AvatarPage.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import AvatarMobile from "../components/avatar/mobile/AvatarMobile";
+import { maleCards, femaleCards } from "../utils/constants";
+// import { maleCardsActual, femaleCardsActual } from "../utils/constantsActual";
+
+export default function AvatarPage({
+  capturedImage,
+  setGeneratedImage,
+  selectedGender,
+  generatedImage,
+}) {
+  const navigate = useNavigate();
+  const [selectedImage, setSelectedImage] = useState();
+  /*   capturedImage && console.log("capturedImage =>", capturedImage.split(",")[1]); */
+  selectedImage && console.log("selectedImage =>", selectedImage.split(",")[1]);
+  /*  console.log("actual=>", maleCardsActual, femaleCardsActual); */
+  selectedGender && console.log(selectedGender);
+  // toast options
+  const toastOptions = {
+    position: "top-left",
+    autoClose: 4000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "light",
+  };
+
+  /*  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+
+  const getScaleImage = (originalImg, sw, sh) => {
+    canvas.width = img.width * sw;
+    canvas.height = img.height * sh;
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    return canvas.toDataURL("image/png");
+  }; */
+
+  // submitting the selected image and post request to api
+  const handleSubmit = () => {
+    console.log("clicked");
+    setGeneratedImage("");
+    if (selectedImage) {
+      axios
+        .post("https://396e-103-17-110-13.ngrok-free.app/rec", {
+          image: capturedImage.split(",")[1],
+          choice: selectedImage.split(",")[1],
+        })
+        .then(function (response) {
+          console.log(response);
+          // getScaleImage()
+
+          setGeneratedImage(`data:image/webp;base64,${response.data.result}`);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      navigate("/generated-image");
+    } else {
+      toast.error("Please select an image...", toastOptions);
+    }
+  };
+  return (
+    <AvatarPageWrapper>
+      <div className="mobile">
+        <AvatarMobile
+          maleCards={maleCards}
+          femaleCards={femaleCards}
+          selectedGender={selectedGender}
+          handleSubmit={handleSubmit}
+          setSelectedImage={setSelectedImage}
+          generatedImage={generatedImage}
+        />
+      </div>
+      <ToastContainer />
+    </AvatarPageWrapper>
+  );
+}
+
+const AvatarPageWrapper = styled.div``;
